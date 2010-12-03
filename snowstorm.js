5a74ca225e075402e55f506960a65869119e255d
Scott Schiller <sm2@schillmania.com>
2010-12-02 22:37:51 -0800

    Dev branch: Experimental CSS @fontface + CSS transforms (2D/3D) features

--- snowstorm.js
+++ snowstorm.js
@@ -33,15 +33,40 @@ var snowStorm = (function(window, document) {
   this.freezeOnBlur = true;       // Only snow when the window is in focus (foreground.) Saves CPU.
   this.flakeLeftOffset = 0;       // Left margin/gutter space on edge of container (eg. browser window.) Bump up these values if seeing horizontal scrollbars.
   this.flakeRightOffset = 0;      // Right margin/gutter space on edge of container
-  this.flakeWidth = 8;            // Max pixel width reserved for snow element
-  this.flakeHeight = 8;           // Max pixel height reserved for snow element
-  this.vMaxX = 5;                 // Maximum X velocity range for snow
-  this.vMaxY = 4;                 // Maximum Y velocity range for snow
+  this.flakeWidth = 15;           // Max pixel width reserved for snow element
+  this.flakeHeight = 15;          // Max pixel height reserved for snow element
+  this.vMaxX = 3;                 // Maximum X velocity range for snow
+  this.vMaxY = 2;                 // Maximum Y velocity range for snow
   this.zIndex = 0;                // CSS stacking order applied to each snowflake
+
+  // --- experimental web font + CSS transform bits ---
+
+  /**
+   * WARNING: These features may not be supported in all browsers.
+   * They may also cause really high CPU use; here be dragons, etc. Tread carefully.
+   */
+
+  // Web font rendering may not be awesome (esp. if font smoothing is off), etc.
+
+  this.useWebFont = true;         // (if supported by browser) use embedded EOT/TTF/WOFF font file for real snowflake characters
+
+  // CSS transforms may be very CPU-heavy if not hardware-accelerated.
+
+  this.use2DRotate = true;        // (if supported by browser) 2D CSS transforms: rotating snow
+  this.use3DRotate = true;        // (if supported by browser) additional 3D CSS transform effect
+
+  this.webFontCharacters = ['%','\'','(','1','7','p','v','w','y','z']; // characters for webflakes font (EOT/TTF/WOFF)
+  this.webFontCharactersSubset = ['%','\'']; // if 2D/3D transforms enabled, may be too slow without hardware acceleration - use these simpler glyphs, lower CPU
+
+  if (!navigator.userAgent.match(/safari/i)) {
+    // "&" character glyph doesn't work in Safari?
+    this.webFontCharacters.push('&');
+    this.webFontCharactersSubset.push('&');
+  }
 
   // --- End of user section ---
 
-  var s = this, storm = this, i,
+  var s = this, storm = this, i, head, css, cssLink, cssText, text, node,
   // UA sniffing and backCompat rendering mode checks for fixed position, etc.
   isIE = navigator.userAgent.match(/msie/i),
   isIE6 = navigator.userAgent.match(/msie 6/i),
@@ -51,19 +76,27 @@ var snowStorm = (function(window, document) {
   noFixed = (isMobile || isBackCompatIE || isIE6),
   screenX = null, screenX2 = null, screenY = null, scrollY = null, vRndX = null, vRndY = null,
   windOffset = 1,
-  windMultiplier = 2,
-  flakeTypes = 6,
+  angle = 0,
+  windMultiplier = 1.1,
   fixedForEverything = false,
+  testDiv = document.createElement('div'),
+  snowTemplate = null,
   opacitySupported = (function(){
     try {
-      document.createElement('div').style.opacity = '0.5';
+      testDiv.style.opacity = '0.5';
     } catch(e) {
       return false;
     }
     return true;
   }()),
   didInit = false,
+  transforms = {
+    moz: (typeof testDiv.style.MozTransform !== 'undefined' ? 'MozTransform' : null),
+    webkit: (typeof testDiv.style.webkitTransform !== 'undefined' ? 'webkitTransform' : null),
+    prop: null
+  },
   docFrag = document.createDocumentFragment();
+  transforms.prop = (transforms.moz || transforms.webkit);
 
   this.timers = [];
   this.flakes = [];
@@ -71,6 +104,7 @@ var snowStorm = (function(window, document) {
   this.active = false;
   this.meltFrameCount = 20;
   this.meltFrames = [];
+  this.types = 16;
 
   this.events = (function() {
 
@@ -128,6 +162,119 @@ var snowStorm = (function(window, document) {
   function plusMinus(n) {
     return (parseInt(rnd(2),10)===1?n*-1:n);
   }
+
+  function checkFontFace() {
+
+    /**
+     * CSS @font-face detection code: Modernizr.js library (BSD/MIT) by Faruk Ates and Paul Irish
+     * http://www.modernizr.com/
+     * Their code credits Diego Perini's CSS support script
+     * http://javascript.nwbox.com/CSSSupport/
+     */
+
+    var sheet,
+        style = document.createElement('style'),
+        impl = document.implementation || { hasFeature: function() { return false; } },
+        supportAtRule;
+
+    style.type = 'text/css';
+    head.insertBefore(style, head.firstChild);
+    sheet = style.sheet || style.styleSheet;
+
+    // removing it crashes IE browsers
+    // head.removeChild(style);
+
+    supportAtRule = impl.hasFeature('CSS2', '') ? function(rule) {
+
+      if (!(sheet && rule)) {
+        return false;
+      }
+      var result = false;
+      try {
+        sheet.insertRule(rule, 0);
+        result = !(/unknown/i).test(sheet.cssRules[0].cssText);
+        sheet.deleteRule(sheet.cssRules.length - 1);
+      } catch(e) {}
+      return result;
+
+    } : function(rule) {
+
+      if (!(sheet && rule)) {
+        return false;
+      }
+      sheet.cssText = rule;
+      return sheet.cssText.length !== 0 && !(/unknown/i).test(sheet.cssText) && sheet.cssText.replace(/\r+|\n+/g, '').indexOf(rule.split(' ')[0]) === 0;
+
+    };
+
+    return supportAtRule('@font-face { font-family: "font"; src: "font.ttf"; }');
+
+  }
+
+  function addWebFont() {
+
+    // now we inject the CSSes (sneaky inline injection for most, external CSS file for IE.)
+
+    /*
+    var css_link = document.createElement('link');
+    css_link.rel = 'stylesheet';
+    css_link.type = 'text/css';
+    css_link.href = 'wwflakes-webfont.css';
+    head.appendChild(css_link);
+    */
+
+    css = document.createElement('style');
+
+    text = {
+      rule: "@font-face",
+      value: [
+        "font-family: 'WWFlakesRegular';",
+        "src: url('wwflakes-webfont.eot');",
+        "src: local('`'), url('wwflakes-webfont.woff') format('woff'), url('wwflakes-webfont.ttf') format('truetype');",
+        "font-weight: normal;",
+        "font-style: normal;"
+      ]
+    };
+
+    /*
+    text = {
+      rule: 'body',
+      value: ['color:#ff33ff']
+    }
+    */
+
+    cssText = text.rule + ' {\n' + text.value.join('\n') + '\n}';
+
+    node = document.createTextNode(cssText);
+
+    if (!isIE) {
+
+      css.appendChild(node);
+      head.appendChild(css);
+
+    } else {
+
+      /*
+        var count = document.styleSheets.length;
+        head.appendChild(css);
+        document.styleSheets[count].addRule(text.rule, text.value.join('\n'));
+        // css.innerHTML = 'body { color: #ff33ff; }'; // fails
+      */
+
+      cssLink = document.createElement('link');
+      cssLink.rel = 'stylesheet';
+      cssLink.type = 'text/css';
+      cssLink.href = 'wwflakes-webfont.css';
+      head.appendChild(cssLink);
+
+    }
+
+  }
+
+  this.getSnowCharacter = function() {
+    var charset = (s.useWebFont ? (s.use2DRotate ? (transforms.webkit ? s.webFontCharacters : s.webFontCharactersSubset) : s.webFontCharactersSubset) : null);
+    return (!charset ? s.snowCharacter : charset[parseInt(Math.random()*charset.length,10)]);
+  };
 
   this.randomizeWind = function() {
     vRndX = plusMinus(rnd(s.vMaxX,0.2));
@@ -235,31 +382,30 @@ var snowStorm = (function(window, document) {
   };
 
   this.SnowFlake = function(parent,type,x,y) {
-    var s = this, storm = parent;
+    var s = this, storm = parent, style = [];
     this.type = type;
     this.x = x||parseInt(rnd(screenX-20),10);
     this.y = (!isNaN(y)?y:-rnd(screenY)-12);
     this.vX = null;
     this.vY = null;
-    this.vAmpTypes = [1,1.2,1.4,1.6,1.8]; // "amplification" for vX/vY (based on flake size/type)
-    this.vAmp = this.vAmpTypes[this.type];
+    this.angle = 0;
+    this.angleOffset = parseInt(Math.random()*360,10);
+    this.angleMultiplier = plusMinus(Math.random()*2);
+    this.rotateDirection = plusMinus(1+plusMinus(Math.random()*1));
+    this.vAmp = 1+(0.1*this.type);
     this.melting = false;
     this.meltFrameCount = storm.meltFrameCount;
     this.meltFrames = storm.meltFrames;
     this.meltFrame = 0;
     this.twinkleFrame = 0;
     this.active = 1;
-    this.fontSize = (10+(this.type/5)*10);
-    this.o = document.createElement('div');
-    this.o.innerHTML = storm.snowCharacter;
-    this.o.style.color = storm.snowColor;
-    this.o.style.position = (fixedForEverything?'fixed':'absolute');
-    this.o.style.width = storm.flakeWidth+'px';
-    this.o.style.height = storm.flakeHeight+'px';
-    this.o.style.fontFamily = 'arial,verdana';
-    this.o.style.overflow = 'hidden';
-    this.o.style.fontWeight = 'normal';
-    this.o.style.zIndex = storm.zIndex;
+    this.fontSize = (9+(this.type/storm.types)*(storm.useWebFont?15:20));
+    this.scale = Math.log(this.fontSize-6);
+    this.o = snowTemplate.cloneNode(true);
+    // this.o.style.outline = '1px solid rgba(255,255,255,0.5)';
+    this.o.innerHTML = storm.getSnowCharacter(); // text instead?
+    this.flakeWidth = parseInt(this.fontSize*(storm.useWebFont?2:1),10); // s.o.offsetWidth;
+    this.flakeHeight = parseInt(this.fontSize*(storm.useWebFont?1:1),10); // s.o.offsetHeight;
     docFrag.appendChild(this.o);
 
     this.refresh = function() {
@@ -273,7 +419,7 @@ var snowStorm = (function(window, document) {
 
     this.stick = function() {
       if (noFixed || (storm.targetElement !== document.documentElement && storm.targetElement !== document.body)) {
-        s.o.style.top = (screenY+scrollY-storm.flakeHeight)+'px';
+        s.o.style.top = (screenY+scrollY-s.flakeHeight)+'px';
       } else if (storm.flakeBottom) {
         s.o.style.top = storm.flakeBottom+'px';
       } else {
@@ -282,6 +428,7 @@ var snowStorm = (function(window, document) {
         s.o.style.bottom = '0px';
         s.o.style.position = 'fixed';
         s.o.style.display = 'block';
+//        s.o.style.lineHeight = '100%';
       }
     };
 
@@ -296,18 +443,38 @@ var snowStorm = (function(window, document) {
       }
     };
 
+    this.applyAngle = function(angle) {
+      if (storm.use2DRotate && s.y >= -s.flakeHeight && (transforms.webkit || this.fontSize >= 14)) {
+        var angle2 = Math.abs(Math.min(3,s.angleOffset+(angle*s.angleMultiplier)));
+        if (!storm.use3DRotate || !transforms.webkit) {
+          style.push('rotate('+angle+'deg)');
+        } else {
+          style.push('rotate('+angle+'deg) rotate3D(0,1,0,'+angle2+'deg)'); // should be hardware-accelerated // s.angleOffset+(angle*s.angleMultiplier)
+        }
+        s.o.style[transforms.prop] = style.join(' ');
+        style = [];
+      }
+    };
+
     this.move = function() {
-      var vX = s.vX*windOffset, yDiff;
+      var vX = s.vX*(windOffset*s.scale), yDiff;
       s.x += vX;
       s.y += (s.vY*s.vAmp);
-      if (s.x >= screenX || screenX-s.x < storm.flakeWidth) { // X-axis scroll check
+      if (s.x >= screenX || screenX-s.x < s.flakeWidth) { // X-axis scroll check
         s.x = 0;
-      } else if (vX < 0 && s.x-storm.flakeLeftOffset < 0-storm.flakeWidth) {
-        s.x = screenX-storm.flakeWidth-1; // flakeWidth;
+      } else if (vX < 0 && s.x-storm.flakeLeftOffset < 0-s.flakeWidth) {
+        s.x = screenX-s.flakeWidth-1; // flakeWidth;
+      }
+      if (storm.use2DRotate && transforms.prop) {
+        s.angle += (5 * windOffset * s.rotateDirection);
+        if (Math.max(s.angle) >= 360) {
+          s.angle = 0;
+        }
+        s.applyAngle(s.angle);
       }
       s.refresh();
-      yDiff = screenY+scrollY-s.y;
-      if (yDiff<storm.flakeHeight) {
+      yDiff = screenY + scrollY - s.y - (storm.useWebFont ? 3 : 0);
+      if (yDiff <= s.flakeHeight) {
         s.active = 0;
         if (storm.snowStick) {
           s.stick();
@@ -342,15 +509,14 @@ var snowStorm = (function(window, document) {
     };
 
     this.setVelocities = function() {
-      s.vX = vRndX+rnd(storm.vMaxX*0.12,0.1);
-      s.vY = vRndY+rnd(storm.vMaxY*0.12,0.1);
+      s.vX = vRndX+rnd(storm.vMaxX*0.2,0.1);
+      s.vY = vRndY+rnd(storm.vMaxY*0.2,0.1);
     };
 
     this.setOpacity = function(o,opacity) {
-      if (!opacitySupported) {
-        return false;
+      if (opacitySupported) {
+        o.style.opacity = opacity;
       }
-      o.style.opacity = opacity;
     };
 
     this.melt = function() {
@@ -361,7 +527,7 @@ var snowStorm = (function(window, document) {
           s.meltFrame++;
           s.setOpacity(s.o,s.meltFrames[s.meltFrame]);
           s.o.style.fontSize = s.fontSize-(s.fontSize*(s.meltFrame/s.meltFrameCount))+'px';
-          s.o.style.lineHeight = storm.flakeHeight+2+(storm.flakeHeight*0.75*(s.meltFrame/s.meltFrameCount))+'px';
+          // s.o.style.lineHeight = 'auto'; // s.flakeHeight+2+(s.flakeHeight*0.75*(s.meltFrame/s.meltFrameCount))+'px';
         } else {
           s.recycle();
         }
@@ -380,11 +546,11 @@ var snowStorm = (function(window, document) {
       s.o.style.padding = '0px';
       s.o.style.margin = '0px';
       s.o.style.fontSize = s.fontSize+'px';
-      s.o.style.lineHeight = (storm.flakeHeight+2)+'px';
+      // s.o.style.lineHeight = 'auto'; // (storm.flakeHeight+2)+'px';
       s.o.style.textAlign = 'center';
       s.o.style.verticalAlign = 'baseline';
-      s.x = parseInt(rnd(screenX-storm.flakeWidth-20),10);
-      s.y = parseInt(rnd(screenY)*-1,10)-storm.flakeHeight;
+      s.x = parseInt(rnd(screenX-s.flakeWidth-20),10);
+      s.y = parseInt(rnd(screenY)*-1,10)-s.flakeHeight;
       s.refresh();
       s.o.style.display = 'block';
       s.active = 1;
@@ -431,9 +597,27 @@ var snowStorm = (function(window, document) {
     }
   };
 
+  this.createTemplate = function() {
+    snowTemplate = document.createElement('div');
+    var o = snowTemplate.style;
+    o.color = storm.snowColor;
+    o.margin = '0px';
+    o.padding = '0px';
+    o.position = (fixedForEverything?'fixed':'absolute');
+    o.fontFamily = 'WWFlakesRegular,arial,verdana,sans-serif'; // "Arial Unicode MS","Lucida Sans Unicode",
+    o.display = 'block';
+    o.overflow = 'hidden';
+    o.fontWeight = 'normal';
+    o.zIndex = storm.zIndex;
+    // this.o.style.width = this.flakeWidth+'px';
+    // this.o.style.height = this.flakeHeight+'px';
+    // this.o.style.verticalAlign = 'bottom'; // ?
+    // this.o.style.textAlign = 'center';
+  };
+
   this.createSnow = function(limit,allowInactive) {
     for (var i=0; i<limit; i++) {
-      s.flakes[s.flakes.length] = new s.SnowFlake(s,parseInt(rnd(flakeTypes),10));
+      s.flakes[s.flakes.length] = new s.SnowFlake(s,parseInt(rnd(storm.types),10));
       if (allowInactive || i>s.flakesMaxActive) {
         s.flakes[s.flakes.length-1].active = -1;
       }
@@ -446,6 +630,18 @@ var snowStorm = (function(window, document) {
   };
 
   this.init = function() {
+
+    if (s.useWebFont) {
+      if (checkFontFace()) {
+        addWebFont();
+      } else {
+        // No support. Fall back to plain text.
+        s.useWebFont = false;
+        // Maybe disable rotate, too?
+      }
+    }
+
+    s.createTemplate();
     for (var i=0; i<s.meltFrameCount; i++) {
       s.meltFrames.push(1-(i/s.meltFrameCount));
     }
@@ -491,6 +687,7 @@ var snowStorm = (function(window, document) {
     if (s.targetElement !== document.documentElement && s.targetElement !== document.body) {
       s.resizeHandler = s.resizeHandlerAlt; // re-map handler to get element instead of screen dimensions
     }
+    head = document.getElementsByTagName('head')[0];
     s.resizeHandler(); // get bounding box elements
     s.usePositionFixed = (s.usePositionFixed && !noFixed); // whether or not position:fixed is supported
     fixedForEverything = s.usePositionFixed;
